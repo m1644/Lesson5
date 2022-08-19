4a5a3743493d0b0f91cabd2e3c5f1138327cdf62
MaxX <maxx-f@mail.ru>
2022-08-19 12:26:02 +0300

    Add Task005

---
+++ Task005/Program.cs
@@ -0,0 +1,23 @@
+﻿// Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент,
+// второй и предпоследний и т.д. Результат запишите в новом массиве.
+
+int[] CreateArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+        Console.Write(array[i] + " ");
+    }
+    return array;
+}
+
+int[] array = CreateArray(10, 0, 10);
+Console.WriteLine();
+
+int j = array.Length - 1;
+for (int i = 0; i < (array.Length + 1) / 2; i++)
+{
+    Console.Write(array[i] * array[j] + " ");
+    j--;
+}
