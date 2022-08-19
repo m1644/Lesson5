2182a3c6b7fd1a455c4682ea80e5491b5a1231c9
MaxX <maxx-f@mail.ru>
2022-08-19 08:44:03 +0300

    Add Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу замена элементов массива: положительные элементы замените
+// на соответствующие отрицательные, и наоборот.
+
+int[] CreateArray(int length, int min, int max)  // Задать массив
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+int[] ChangeArray(int[] array)  // Замена положительных элементов отрицательными и наоборот
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * -1;
+    }
+    return array;
+}
+
+void PrintArray(int[] array)  // Вывод
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array [i] + " ");
+}
+
+int[] array = CreateArray(4, -10, 10);
+PrintArray(array);
+Console.WriteLine();
+ChangeArray(array);
+PrintArray(array);
